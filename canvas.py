f75f369c8ff5e0b12cd20b50e5fe59f0e312d40e
MrSuave <cromeo5112@gmail.com>
2022-05-12 11:08:03 -0400

    l

--- canvas.py
+++ canvas.py
@@ -51,130 +51,25 @@ Grade Data:
 }
 """
 
-with open("config.json") as f:
-    config = json.load(f)
+os.chdir('1865191/')
+with open("inp.json") as f:
+    settings = json.load(f)
 
-token = config["login_token"]
-root = config["root"]
-course_data = config["course_data"]
-
+token = settings["login_token"]
 canvasAPI = CanvasAPI(token)
 
-def updateCanvasGroups(course_id, group_data):
-    for group in group_data:
-        data = {
-            "name":group,
-            "group_weight":group_data[group]['group_weight'],
-            "rules":group_data[group]['rules']
-        }
-        canvasAPI.updateGroup(course_id,group_data[group]['id'],data)
-
-def updateLocalGroups(course_id):
-
-    course_name = (course for course in os.listdir(root) if course_data[course]['id'] == course_id)
-    course_name = next(course_name)
-    current_group_data = course_data[course_name]["group_data"]
+def init_course():
+    dirs = (dir for dir in os.listdir() if dir != 'inp.json')
+    for dir in dirs:
+        dir_settings = settings[dir]
+        print(dir_settings)
     
-    canvas_group_data = canvasAPI.getCourseGroups(course_id)
-    
-    new_group_data = {
-        canvas_group["name"]:{
-            "group_weight":canvas_group["group_weight"],
-            "default_points":10,
-            "rules":canvas_group["rules"],
-            "id":canvas_group["id"]
-        } for canvas_group in canvas_group_data}
-   
-    for group in new_group_data:
-        
-        rules = ""
-        for rule in new_group_data[group]["rules"]:
-            rules += f'{rule}:{new_group_data[group]["rules"][rule]}\n'
-        new_group_data[group]["rules"] = rules
-        
-        if group in current_group_data:
-            new_group_data[group]["default_points"] = current_group_data[group]["default_points"]
-    
-    course_data[course_name]['group_data'] = new_group_data
-    config['course_data'] = course_data
-    
-    with open("config.json",'w') as f:
-        json.dump(config,f,indent=2)
-    
-    return new_group_data
-
-def cloneDir():
-    
-    courses = os.listdir(root)
-    for course in courses:
-
-        course_id = course_data[course]['id']
-        groups = os.listdir(f'{root}\\{course}')
-        group_data = course_data[course]["group_data"]
-
-        canvasAPI.enableGroupWeights(course_id)
-
-        for group in groups:
-            
-            if group not in group_data:
-                
-                group_data = updateLocalGroups(course_id)
-                
-                if group not in group_data:
-                    
-                    temp_group_data = {
-                        'name':group
-                    }
-
-                    
-                    temp_group = canvasAPI.createGroup(course_id, temp_group_data).json()
-                    
-                    group_data = updateLocalGroups(course_id)
-
-            group_id = group_data[group]['id']
-            
-            assignments = os.listdir(f'{root}\\{course}\\{group}')
-            for assignment in assignments:
-
-                data = {
-                'assignment[name]': assignment,
-                'assignment[points_possible]' : group_data[group]['default_points'],
-                'assignment[published]' : False,
-                'assignment[assignment_group_id]': group_data[group]['id']
-                }
-                file_data = os.listdir(f'{root}\\{course}\\{group}\\{assignment}')
-                for file in file_data:
-                    if '.pdf' in file:
-                        response = canvasAPI.uploadFile(course_id, file, f'{root}\\{course}\\{group}\\{assignment}\\{file}', {'name':file,'parent_folder_path':group})
-                        file_id = response.json()['id']
-                        file_preview = f'<p><a class="instructure_file_link instructure_scribd_file auto_open" title="{file}" href="https://bridgeport.instructure.com/courses/{course_id}/files/{file_id}?wrap=1" target="_blank" rel="noopener" data-api-endpoint="https://bridgeport.instructure.com/api/v1/courses/{course_id}/files/{file_id}" data-api-returntype="File">{file}</a></p>'
-                        data['assignment[description]'] = file_preview
-                        break
-
-                canvasAPI.createAssignment(course_id,data)
 
 def main():
-    
-    course_id = 1865191
-    ali_student_id = 5499008
-    
+    init_course()
     # & C:/Users/Romeo/anaconda3/python.exe "c:/Users/Romeo/Desktop/Canvas API/canvas.py"  cmd
-    
-    # try:
-    #     arg = sys.argv[1]
-    #     print(arg)
-    # except IndexError:
-    #     pass
 
-    data = {
-        "assignment[name]" : "Test Assignment", # str
-        "assignment[points_possible]" : 100, # int
-        "assignment[assignment_group_id]" : 3119489, # int
-        "assignment[published]" : True # boolean
-    }
-    response = canvasAPI.createAssignment(course_id,data).json()
-    assignment_id = response['id']
-    canvasAPI.gradeAssignment(course_id, assignment_id, ali_student_id, {"submission[posted_grade]" : "86"})
+
 
 if __name__ == "__main__":
     main()
